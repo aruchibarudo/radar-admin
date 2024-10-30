ee9974d228c2f8ca74b11e25dd12c8231c31aa5d
Dmitry V <dmitry.share@gmail.com>
2024-10-31 02:00:21 +0300

    fix Next.js prerender build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 'use client'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
 
 import { Button } from '@consta/uikit/Button'
-import { Grid, GridItem } from '@consta/uikit/Grid'
+// import { Grid, GridItem } from '@consta/uikit/Grid'
 import { IconAdd } from '@consta/icons/IconAdd'
 
 import RadarsList from '@/components/radars/RadarsList'
 import Stack from '@/components/ui/container/Stack'
 import { H1 } from '@/components/ui/Text'
+
+// fix Next.js prerender with dynamic import
+const Grid = dynamic(
+  () => import('@consta/uikit/Grid').then((mod) => mod.Grid),
+  {
+    ssr: false,
+  },
+)
+
+const GridItem = dynamic(
+  () => import('@consta/uikit/Grid').then((mod) => mod.GridItem),
+  {
+    ssr: false,
+  },
+)
 
 export default function Home() {
   const router = useRouter()
--- app/radars/new/page.tsx
+++ app/radars/new/page.tsx
@@ -1,13 +1,29 @@
 'use client'
+import dynamic from 'next/dynamic'
 import Link from 'next/link'
 
 import { Breadcrumbs } from '@consta/uikit/Breadcrumbs'
-import { Grid, GridItem } from '@consta/uikit/Grid'
+// import { Grid, GridItem } from '@consta/uikit/Grid'
 import { IconEdit } from '@consta/icons/IconEdit'
 import { IconHome } from '@consta/icons/IconHome'
 
 import RadarForm from '@/components/radars/form/RadarForm'
 import { H1 } from '@/components/ui/Text'
+
+// fix Next.js prerender with dynamic import
+const Grid = dynamic(
+  () => import('@consta/uikit/Grid').then((mod) => mod.Grid),
+  {
+    ssr: false,
+  },
+)
+
+const GridItem = dynamic(
+  () => import('@consta/uikit/Grid').then((mod) => mod.GridItem),
+  {
+    ssr: false,
+  },
+)
 
 const CreateRadarPage = () => {
   return (
